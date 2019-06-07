0689b23bccc1cfb37538bd4c3e384eb022a642b1
Manish Warang <Manish_Warang@infosys.com>
2019-06-07 12:21:39 +0530

    Added more test caes

---
+++ src/test/java/com/g33kzone/sbjunit/controller/MainControllerTest.java
@@ -0,0 +1,32 @@
+package com.g33kzone.sbjunit.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class MainControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getHello() throws Exception{
+        
+        ResponseEntity<String> response = restTemplate.getForEntity(
+            new URL("http://localhost:" + port + "/hello").toString(), String.class);
+
+            assertEquals("Hello JUnit 5", response.getBody());
+    }
+}

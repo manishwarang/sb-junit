b278f78b2f72fd9faaf0a5696a5bdc50d75a2e8f
Manish Warang <Manish_Warang@infosys.com>
2019-06-11 17:40:25 +0530

    Added custom step

---
+++ sonar-qg.sh
@@ -0,0 +1,15 @@
+sonar_host_name=$1
+project_key=$2
+
+ce_status=$(curl -s ${sonar_host_name}/api/qualitygates/project_status?projectKey=${project_key} | jq --raw-output '.projectStatus.status')
+
+echo ${sonar_host_name}
+echo "QG Script --> Status of SonarQube task is ${ce_status}"
+
+if [ ${ce_status} != "OK" ]; then
+  echo "QG Script --> Quality gate is not OK - exiting with error"
+  exit 1
+else
+  echo "QG Script --> Quality gate is OK - exiting with Success"
+  exit 0
+fi

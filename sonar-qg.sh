b42e2be33eef41bebb6687f41ac39117d677b0a4
Manish Warang <Manish_Warang@infosys.com>
2019-06-13 13:21:57 +0530

    updated script

--- sonar-qg.sh
+++ sonar-qg.sh
@@ -11,13 +11,10 @@ jq --version
 
 ce_status=$(curl -s ${sonar_host_name}/api/qualitygates/project_status?projectKey=${project_key} | jq --raw-output '.projectStatus.status')
 
-echo ${sonar_host_name}
-echo "QG Script --> Status of SonarQube task is ${ce_status}"
-
 if [ ${ce_status} != "OK" ]; then
-  echo "QG Script --> Quality gate is not OK - exiting with error"
+  echo "QG Script --> Quality gate is "${ce_status}
   exit 1
 else
-  echo "QG Script --> Quality gate is OK - exiting with Success"
+  echo "QG Script --> Quality gate is "${ce_status}
   exit 0
 fi
